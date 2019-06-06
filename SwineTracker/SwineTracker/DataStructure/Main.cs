504de5b61c331683b966384d755de4a16a39d377
AlexRod97 <rodriguezalex86@hotmail.com>
2019-06-06 12:11:20 -0600

    Manejo de datos
    Added code to manage data

---
+++ SwineTracker/SwineTracker/DataStructure/Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwineTracker.Data_Structure
+{
+   public class Main
+    {
+        private string fileName = @"D:\\Escritorio\\Master.txt";
+
+
+        public void Insert(string data)
+        {
+            using (StreamWriter sw = (File.Exists(fileName)) ? File.AppendText(fileName) : File.CreateText(fileName))
+            {
+                sw.WriteLine(data);
+            }
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update(string updatedData, string oldData)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            string tempName = @"D:\\Escritorio\\temp.txt";
+            StreamWriter sw = new StreamWriter(tempName);
+
+            while (!sr.EndOfStream)
+            {
+                string old = sr.ReadLine();
+
+                if (old != oldData)
+                {
+                    sw.WriteLine(old);
+                }
+                else if (old == oldData)
+                {
+                    sw.WriteLine(updatedData);
+                }
+            }
+            sr.Close();
+            sw.Close();
+            File.Delete(fileName);
+            File.Move(tempName, fileName);
+        }
+
+        public void Delete(string deleteData)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            string tempName = @"D:\\Escritorio\\temp.txt";
+            StreamWriter sw = new StreamWriter(tempName);
+
+            while (!sr.EndOfStream)
+            {
+                string old = sr.ReadLine();
+
+                if (old != deleteData)
+                {
+                    sw.WriteLine(old);
+                }
+                else if (old == deleteData)
+                {
+                    continue;
+                }
+            }
+            sr.Close();
+            sw.Close();
+            File.Delete(fileName);
+            File.Move(tempName, fileName);
+        }
+    }
+}
--- SwineTracker/SwineTracker/Form1.cs
+++ SwineTracker/SwineTracker/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SwineTracker.Data_Structure;
+
 
 namespace SwineTracker
 {
     public partial class Form1 : Form
     {
+        Main dataStructure = new Main();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataStructure.Insert(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataStructure.Update(textBox2.Text, textBox1.Text);
+            MessageBox.Show("Actualizado");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataStructure.Delete(textBox1.Text);
+            MessageBox.Show("Eliminado");
         }
     }
 }
